170ced93942c7613d88d06f6f3438c82ed729088
Weebadu-arachchige-pathum-Vimukthi <120662772+Weebadu-arachchige-pathum-Vimukthi@users.noreply.github.com>
2024-11-07 14:04:12 +0530

    first page code edit 4

--- behave.js
+++ behave.js
@@ -15,3 +15,38 @@ window.onclick = function(event) {
         }
     }
 }
+function adjustForMobile() {
+  if (window.innerWidth <= 768) {
+    document.body.style.backgroundColor = "#e0f7fa"; 
+  } else {
+    document.body.style.backgroundColor = "#f0f0f0"; 
+  }
+}
+
+window.addEventListener("resize", adjustForMobile);
+window.addEventListener("load", adjustForMobile);
+const nameText = "Pathum Vimukthi";
+let index = 0;
+
+
+function typeEffect() {
+    if (index < nameText.length) {
+        document.getElementById("nameDisplay").innerHTML += nameText.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100); 
+    } else {
+     
+        setTimeout(() => {
+            document.getElementById("loadingScreen").style.animation = "fadeOut 1s forwards";
+            setTimeout(() => {
+                document.getElementById("loadingScreen").style.display = "none";
+                document.getElementById("mainContent").style.display = "block";
+            }, 1000); 
+        }, 1000); 
+    }
+}
+
+
+window.addEventListener("load", () => {
+    typeEffect();
+});
